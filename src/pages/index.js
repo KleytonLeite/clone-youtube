7869a572c056f0fc6cb759a1130e6f3ebeb92a08
Kleyton Leite <musicpteclados@gmail.com>
2021-02-03 12:01:47 -0300

    Configuração do Material-UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react';
+import { Button } from '@material-ui/core';
+
 
 export default function Home() {
-  return <div>Clone youtube com next.js + Material-UI</div>;
+  return (
+        <div>
+          Clone youtube com next.js + Material-UI
+          <Button variant="contained" color="secondary">Primary</Button>
+        </div>
+  )
 }
